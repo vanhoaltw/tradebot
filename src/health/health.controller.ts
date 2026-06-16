aed2606411544070756b99e7b44962b4f677eacb
Nguyễn Vạn Hoà <nguyenvanhoa@MacBook-Pro-cua-Nguyen.local>
2026-06-16 11:46:47 +0700

    feat: add health check endpoint

---
+++ src/health/health.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+import { RedisHealthIndicator } from './redis.health';
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+    private readonly redis: RedisHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.db.pingCheck('database'),
+      () => this.redis.pingCheck('redis'),
+    ]);
+  }
+}
