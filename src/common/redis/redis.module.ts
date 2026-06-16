61de56ee5d5f1cb2f7d19d96e75c92315b8ad3d8
Nguyễn Vạn Hoà <nguyenvanhoa@MacBook-Pro-cua-Nguyen.local>
2026-06-16 11:46:04 +0700

    feat: add shared ioredis client module

---
+++ src/common/redis/redis.module.ts
@@ -0,0 +1,28 @@
+import { Global, Module, OnApplicationShutdown } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ModuleRef } from '@nestjs/core';
+import Redis from 'ioredis';
+import { REDIS_CLIENT } from './redis.constants';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: REDIS_CLIENT,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) =>
+        new Redis(config.get<string>('REDIS_URL', { infer: true }) as string, {
+          maxRetriesPerRequest: null,
+        }),
+    },
+  ],
+  exports: [REDIS_CLIENT],
+})
+export class RedisModule implements OnApplicationShutdown {
+  constructor(private readonly moduleRef: ModuleRef) {}
+
+  async onApplicationShutdown(): Promise<void> {
+    const client = this.moduleRef.get<Redis>(REDIS_CLIENT);
+    await client.quit();
+  }
+}
